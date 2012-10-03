cecafd615d9f15f4441ebbd1003e6c10a00e863d
Oren Forer <oren@junctionnetworks.com>
2012-10-03 11:05:30 -0400

    volume control: input and playback

--- src/com/onsip/felix/privileged/Generic.java
+++ src/com/onsip/felix/privileged/Generic.java
@@ -11,20 +11,20 @@ import com.onsip.felix.AppletLauncher;
 import com.onsip.felix.exceptions.NoSuchApiFunction;
 import com.onsip.felix.exceptions.ParamsDoNotMatchException;
 
-/** 
+/**
  * @author Junction Networks
  */
 public class Generic
     implements PrivilegedAction<Object>
 {
-    private final static Logger m_logger = 
+    private final static Logger m_logger =
         Logger.getLogger(Generic.class.getName());
-    
+
     public enum Exported_API
     {
-        REGISTER, UNREGISTER, REREGISTER, 
+        REGISTER, UNREGISTER, REREGISTER, INPUT_VOLUME, PLAYBACK_VOLUME,
         CALL_CREATE, CALL_TERMINATE, CALL_REQUESTED, INVITE, TRANSFER_CALL,
-        SEND_TONE, MUTE, HOLD, DEFAULT_AUDIO_DEVICE, VOLUME; 
+        SEND_TONE, MUTE, HOLD, DEFAULT_AUDIO_DEVICE;
     }
 
     private String fn;
@@ -32,70 +32,74 @@ public class Generic
 
     public Generic(String fnName, String [] args)
     {
-        this.fn = fnName;        
-        this.args = args;        
+        this.fn = fnName;
+        this.args = args;
     }
-   
+
     @Override
     public String run()
     {
         try
-        {                        
+        {
             Object service = AppletLauncher.getService();
-                        
+
             Method m = parseAPI(service);
             if (m == null)
             {
                 throw new NoSuchApiFunction();
             }
-            
+
             Class<?>[] params = m.getParameterTypes();
             int len = params.length;
             if (len != this.args.length)
             {
                 throw new ParamsDoNotMatchException();
             }
-            Object [] argos = new Object[len];            
+            Object [] argos = new Object[len];
             for (int i=0; i < len; i++)
-            {                
+            {
                 if (params[i].getName().equals("char"))
-                {                                        
-                    argos[i] = new Character(this.args[i].charAt(0)); 
+                {
+                    argos[i] = new Character(this.args[i].charAt(0));
                 }
                 else if (params[i].getName().equals("java.lang.Boolean"))
                 {
                     argos[i] = new Boolean(this.args[i]);
                 }
+                else if (params[i].getName().equals("java.lang.Integer"))
+                {
+                    argos[i] = new Integer(this.args[i]);
+                }
                 else
                 {
                     // default String
                     argos[i] = this.args[i];
-                }                
-            }                 
+                }
+            }
             Object returnType = m.invoke(service, argos);
             if (returnType != null && returnType.getClass() != Void.class)
             {
                 m_logger.info("Return is " + returnType.toString());
                 JSObject win = AppletLauncher.getJSObject();
                 if (win != null)
-                {                
+                {
                     String [] ret = new String[1];
                     ret[0] = returnType.toString();
                     win.call(AppletLauncher.JS_FN_EXPORTED, ret); 
-                } 
+                }
             }
-        }        
+        }
         catch (Exception e)
         {
             m_logger.log(Level.SEVERE, e.getMessage(), e);
         }
         return null;
     }
-        
-    private Method parseAPI(Object service) 
+
+    private Method parseAPI(Object service)
         throws SecurityException, NoSuchMethodException
     {
-        Method m = null;        
+        Method m = null;
         switch (Exported_API.valueOf(fn))
         {
             case REGISTER:
@@ -115,7 +119,7 @@ public class Generic
             case UNREGISTER:
                 m = Unregister.getMethod(service);
                 break;
-            case CALL_CREATE:                
+            case CALL_CREATE:
                 if (this.args.length == 2)
                 {
                     m = Call.getMethod(service);
@@ -123,17 +127,17 @@ public class Generic
                 else
                 {
                     m = Call.getMethodWithSetupId(service);
-                }                
+                }
                 break;
             case CALL_TERMINATE:
                 if(this.args.length == 1)
                 {
                     m = Terminate.getMethodForCallId(service);
-                }   
+                }
                 else
                 {
                     m = Terminate.getMethodForCallIdAndPeerId(service);
-                }                
+                }
                 break;
             case CALL_REQUESTED:
                 if (this.args.length == 0)
@@ -143,9 +147,9 @@ public class Generic
                 else if(this.args.length == 1)
                 {
                     m = PickupCall.getMethodForCallId(service);
-                }                
-                break;    
-            case SEND_TONE:                
+                }
+                break;
+            case SEND_TONE:
                 if (this.args.length == 1)
                 {
                     m = DtmfKeyPress.getMethod(service);
@@ -155,7 +159,7 @@ public class Generic
                     m = DtmfKeyPress.getMethodForCallId(service);
                 }
                 else if (this.args.length == 3)
-                {                    
+                {
                     m = DtmfKeyPress.getMethodForStartStop(service);
                 }
                 break;
@@ -166,9 +170,9 @@ public class Generic
                 }
                 else if(this.args.length == 2)
                 {
-                    m = Mute.getMethodForCallId(service);                    
-                }               
-                break;              
+                    m = Mute.getMethodForCallId(service);
+                }
+                break;
             case HOLD:
                 if (this.args.length == 1)
                 {
@@ -191,11 +195,15 @@ public class Generic
                 break;
             case DEFAULT_AUDIO_DEVICE:
                 m = DefaultAudioDevice.getMethod(service);
-                break;                     
-            case VOLUME:
-                break;                        
+                break;
+            case INPUT_VOLUME:
+                m = InputVolume.getMethod(service);
+                break;
+            case PLAYBACK_VOLUME:
+                m = PlaybackVolume.getMethod(service);
+                break;
         }
-        
+
         return m;
     }
 }
